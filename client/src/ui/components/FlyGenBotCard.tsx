3de04ad7a12497f227fd58f2145ba97aaa9d7329
rasla <programmer.rasla@gmail.com>
2024-07-26 13:35:23 +0530

    add

---
+++ client/src/ui/components/FlyGenBotCard.tsx
@@ -0,0 +1,23 @@
+type Props = {
+  heading: string;
+  subHeading: string;
+};
+
+export const FlyGenBotCard = ({ heading, subHeading }: Props) => {
+  return (
+    <div
+      className="flex flex-col items-center justify-center w-[400px] rounded-xl border"
+      style={{
+        boxShadow: "rgb(255 202 0) 0px 7px 0px",
+      }}
+    >
+      <div className="w-full h-full flex flex-col p-5 bg-white rounded-xl">
+        <h1 className="text-2xl font-medium capitalize mb-2 break-before-avoid">
+          {heading}
+        </h1>
+
+        <p className="text-sm text-gray-400 leading-normal">{subHeading}</p>
+      </div>
+    </div>
+  );
+};
--- client/src/ui/sections/FlyGenBotSection.tsx
+++ client/src/ui/sections/FlyGenBotSection.tsx
@@ -1,25 +1,33 @@
+import { FlyGenBotCard } from "../components/FlyGenBotCard";
+
 export const FlyGenBotSection = () => {
   return (
     <section className="bg-white text-black font-base h-screen relative flex justify-center items-center">
       <div className="relative">
         <img
-          src="/img/3rd-section.png"
-          className="w-[1200px]"
+          src="/img/genbot-img.png"
+          className="w-[800px]"
           alt="Fly Genbot"
           style={{
             zIndex: 1,
-          }}
-        />
-
-        <img
-          src="/img/overlay.svg"
-          className="absolute top-0 left-0 w-[1300px]"
-          alt="Fly Genbot"
-          style={{
-            zIndex: 2,
+            transform: "scaleX(-1)",
           }}
         />
       </div>
+
+      <div className="absolute bg-white z-0 top-10 left-1/2">
+        <FlyGenBotCard
+          heading="Hazardous environment compatibility"
+          subHeading="Designed to excel in toxic and hazardous settings, Genbot ensures human safety."
+        />
+      </div>
+
+      {/* <div className="absolute bg-white z-0 top-10 right-1/3">
+        <FlyGenBotCard
+          heading="Hazardous environment compatibility"
+          subHeading="Designed to excel in toxic and hazardous settings, Genbot ensures human safety."
+        />
+      </div> */}
     </section>
   );
 };
